c3b558d45eb9b5a1c665ac57c5365b38948362d6
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-03-03 20:32:32 -0300

    javaScript com Objetos

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// document.write('Olá, Mundo!' + Date());
+
+function acendeLampada() {
+    document.getElementById('luz').src = "https://images.pexels.com/photos/3651820/pexels-photo-3651820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+}
+
+function apagaLampada() {
+    document.getElementById('luz').src = "https://images.pexels.com/photos/4792503/pexels-photo-4792503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+}
+
+function lampadaQuebrada() {
+    document.getElementById('luz').src = "https://images.pexels.com/photos/128665/pexels-photo-128665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+}
