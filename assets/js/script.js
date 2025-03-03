fbafe410af6577ecdb77477edf0c90b36a6cdba0
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-03-03 20:51:46 -0300

    funcoes javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 // document.write('Olá, Mundo!' + Date());
+var quebrada = false;
 
-function acendeLampada() {
-    document.getElementById('luz').src = "https://images.pexels.com/photos/3651820/pexels-photo-3651820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
-}
+function mudaLampada(tipo) {
+    if (tipo == 1) {
+        arquivo = "https://images.pexels.com/photos/3651820/pexels-photo-3651820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1"
+    }
 
-function apagaLampada() {
-    document.getElementById('luz').src = "https://images.pexels.com/photos/4792503/pexels-photo-4792503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
-}
+    if (tipo == 2) {
+        arquivo = "https://images.pexels.com/photos/4792503/pexels-photo-4792503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+    }
 
-function lampadaQuebrada() {
-    document.getElementById('luz').src = "https://images.pexels.com/photos/128665/pexels-photo-128665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+    if (tipo == 3) {
+        arquivo = "https://images.pexels.com/photos/128665/pexels-photo-128665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=360&dpr=1";
+    }
+
+    if (!quebrada) {
+        document.getElementById('luz').src = arquivo;
+        if (tipo == 3) {
+            quebrada = true;
+        }
+    }
+
 }
